c07727685b2b4e50cdc5a7c25650c8f3fef98c39
shinsukematsuda <smatsuda@x-hack.jp>
2022-07-18 22:44:50 +0900

    フォーマット修正

--- app/views/api/v1/menu/categories/index.json.jbuilder
+++ app/views/api/v1/menu/categories/index.json.jbuilder
@@ -1,1 +1,10 @@
-json.array! @categories
+json.data do
+  json.services do
+    json.array! @categories do |category|
+      json.id category.id
+      json.label category.name
+    end
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace 'api' do
+  namespace 'api', defaults: { format: :json } do
     namespace 'v1' do
       resources :users, only: [:index, :create]
       namespace :menu do
